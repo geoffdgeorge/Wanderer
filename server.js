802e0d2a5290ded69d9d5733068e464f8b54d52c
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-04-23 11:57:53 -0500

    Refactored frontEnd.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require(`./backend/apiRoutes`);
 
 const app = express();
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
